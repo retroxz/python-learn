564c13892a6ab945cee85528784b0cef64579380
retroxz <zzxee666@gmail.com>
2021-01-12 21:55:24 +0800

    :sparkles: base variable

---
+++ variable/string.py
@@ -0,0 +1,8 @@
+# str = '''hello
+#     retro
+#     hello'''
+print(str)
+
+# 转义字符
+print('hello \\n world')
+print('retro'[-2:])
---
+++ variable/var.py
@@ -0,0 +1,24 @@
+# a = 1
+# b = -1
+# c = 1.11111111111111111111
+# print(type(c))
+"""
+print(1//1)
+print(1/1)
+"""
+# 进制
+'''
+print(0b11)
+print(0o11)
+print(bin(114514))
+print(oct(114514))
+print(hex(114514))
+print(int(114514))
+'''
+print(int(True))
+print(int(False))
+
+print(bool(0))
+print(bool(-1))
+print(bool(1))
+print(bool(''))
